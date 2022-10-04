aa654cccd6377c69ff9312588139430a452feeb5
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-10-03 20:27:37 -0700

    updated hw5

--- ch5/ch5Hw.py
+++ ch5/ch5Hw.py
@@ -1,34 +1,34 @@
 # Exercises 5.3, 5.15, 5.17, 5.29
 
 # 5.3 Conversion from kilograms to pounds
-# print(f"{'Kilograms':<13}{'Pounds':<10}")
-# print()
-# conversionNum = 2.2
+print(f"{'Kilograms':<13}{'Pounds':<10}")
+print()
+conversionNum = 2.2
 
-# for i in range(1,200):
-#     print(f"{i :< 13}{round(i * conversionNum, 1) :<10}")
+for i in range(1,200):
+    print(f"{i :< 13}{round(i * conversionNum, 1) :<10}")
 
 # 5.15 Find the largest n such that n^3 < 12,000
-# largestNum = -1
-# i = 0
-# while pow(i, 3) < 12000:
-#     if i > largestNum:
-#         largestNum = i
-#     i+=1
+largestNum = -1
+i = 0
+while pow(i, 3) < 12000:
+    if i > largestNum:
+        largestNum = i
+    i+=1
 
-# print(largestNum)
+print(largestNum)
 
 # 5.17 Display ASCII character table
 
-# start = ord("!")
-# end = ord("~") + 1
-# count = 0
-# for i in range(start, end):
-#     count+=1
-#     if count % 10 == 0:
-#         print(chr(i), end="\n")
-#     else:
-#         print(chr(i), end=" ")
+start = ord("!")
+end = ord("~") + 1
+count = 0
+for i in range(start, end):
+    count+=1
+    if count % 10 == 0:
+        print(chr(i), end="\n")
+    else:
+        print(chr(i), end=" ")
 
 # 5.29 Display Leap Years
 
