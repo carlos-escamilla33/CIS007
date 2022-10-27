234c38681dc1275d767f649dfde4dccfbf34058e
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-10-27 10:31:19 -0700

    updated exercise 7.3

--- ch7_8/ch7_8.py
+++ ch7_8/ch7_8.py
@@ -3,37 +3,47 @@
 # Carlos Rodriguez Escamilla
 # 10/26/2022
 
-# 7.3 How do you create an object?
+# 7.3 The Account class
+
+# UML Diagram
+
+
 
 class Account:
-    def __init__ (self, id = 0, balance = 100.0, annualInterestRate = 10.0):
+    def __init__ (self, id = 0, balance = 100.0, annualInterestRate = 0.0):
         self.__id = id
         self.__balance = balance
         self.__annualInterestRate = annualInterestRate
-    
+
     def getId(self):
         return self.__id
     
     def getBalance(self):
         return self.__balance
     
-    def getAnnualInterestRate(self):
+    def getAnnualInterest(self):
         return self.__annualInterestRate
     
     def setId(self, id = 0):
         self.__id = id
     
-    def setBalance(self, balance = 0):
+    def setBalance(self, balance = 0.0):
         self.__balance = balance
-    
+
     def setAnnualInterestRate(self, annualInterestRate):
         self.__annualInterestRate = annualInterestRate
-    
-    def credit(self, amount = 0):
-        self.__balance = self.__balance + amount
-    
-    def debit(self, amount = 0):
-        self.__balance = self.__balance - amount
+
+    def getMonthlyInterestRate(self):
+        return round(self.__annualInterestRate / 12, 2) / 100
     
     def getMonthlyInterest(self):
-        return self.__balance * (self.__annualInterestRate / 12)
+        return self.__balance * (self.getMonthlyInterestRate())
+    
+    def withdraw(self, amount = 0):
+        self.balance = self.__balance - amount
+    
+    def deposit(self, amoount = 0):
+        self.balance = self.balance + amoount
+
+    
+
