e5c184a6df3d9205e98bfe6b2f6fa25920703b27
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-11-07 00:05:49 -0800

    working on 10.8

--- ch10_11/ch10_11.py
+++ ch10_11/ch10_11.py
@@ -20,13 +20,34 @@ def gradeScore(score, bestScore):
     
     return grade
     
+# def main():
+#     # scores = input("Enter scores:")
+#     # studentScores = [eval(x) for x in scores.split()]
+#     studentScores = [40, 55, 70, 58]
+#     bestScore = max(studentScores)
+#     for i in range(len(studentScores)):
+#         grade = gradeScore(studentScores[i], bestScore)
+#         print(f"Student {i} score is {studentScores[i]} and grade is {grade}")
+
+# main()
+
+
+# 10.8 Find the index of the smallest element
+
+def indexOfSmallestElement(lst = []):
+    if len(lst) > 1:
+        currSmallestIdx = 0
+        for i in range(1, len(lst)):
+            if lst[i] < lst[currSmallestIdx]:
+                currSmallestIdx = i
+        
+        return currSmallestIdx
+
 def main():
-    # scores = input("Enter scores:")
-    # studentScores = [eval(x) for x in scores.split()]
-    studentScores = [40, 55, 70, 58]
-    bestScore = max(studentScores)
-    for i in range(len(studentScores)):
-        grade = gradeScore(studentScores[i], bestScore)
-        print(f"Student {i} score is {studentScores[i]} and grade is {grade}")
+    userInput = input("Enter a list of numbers (comma separated): ")
+    numsLst = [eval(num) for num in userInput.split(",")]
+    lowestIdxResult = indexOfSmallestElement(numsLst)
+
+    print(f"Index {lowestIdxResult} holds the lowest number in the list {numsLst}")
 
 main()
